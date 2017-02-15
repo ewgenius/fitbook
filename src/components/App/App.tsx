7ece0d5e8070af131c277b8e3beba58ab5e654fe
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-02-15 10:51:12 +0300

    upd

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import * as React from 'react';
+import { Component } from 'react';
+
+export default class App extends Component<any, any> {
+  render() {
+    return (
+      <div className='app'>
+      </div>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
+import './styles/index.scss';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.scss';
+import { initializeApp } from 'firebase';
+import App from './components/App/App';
+
+const config = {
+  apiKey: 'AIzaSyB4x88S8zzJfX55cSuCIjDXHOW8MHpEad4',
+  authDomain: 'fitbook-e417b.firebaseapp.com',
+  databaseURL: 'https://fitbook-e417b.firebaseio.com',
+  storageBucket: 'fitbook-e417b.appspot.com',
+  messagingSenderId: '833904904674'
+};
+
+const app = initializeApp(config);
 
 ReactDOM.render(
   <App />,
