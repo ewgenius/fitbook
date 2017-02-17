8e1ab9b66c52fd08b6388f5d59c5560140c0f195
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-02-17 17:54:37 +0300

    welcome screen flow

--- src/components/View/View.tsx
+++ src/components/View/View.tsx
@@ -2,26 +2,10 @@ import * as React from 'react';
 import { Component, Props, HTMLAttributes, CSSProperties } from 'react';
 import AppBar from 'material-ui/AppBar';
 
-interface ViewProps extends Props<View>, HTMLAttributes<HTMLDivElement> {
-  flex?: number;
-  flexFlow?: 'column' | 'row';
-  fill?: boolean;
-}
+interface ViewProps extends Props<View>, HTMLAttributes<HTMLDivElement> {}
 
 export default class View extends Component<ViewProps, {}> {
   render() {
-    const style = {
-      ...this.props.style
-    };
-    if (this.props.flex) style.flex = this.props.flex;
-    if (this.props.flexFlow) {
-      style.flexFlow = this.props.flexFlow;
-      style.display = 'flex';
-    }
-    if (this.props.fill) {
-      style.width = '100%';
-      style.height = '100%';
-    }
-    return <div {...this.props as HTMLAttributes<HTMLDivElement>} style={style}>{this.props.children}</div>;
+    return <div {...this.props as HTMLAttributes<HTMLDivElement>}>{this.props.children}</div>;
   }
 }
--- src/views/WelcomeView/WelcomeView.tsx
+++ src/views/WelcomeView/WelcomeView.tsx
@@ -1,36 +1,70 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { Component, CSSProperties } from 'react';
 import { theme } from '../../theme';
 import SwipeableViews from 'react-swipeable-views';
 import View from '../../components/View/View';
 import FlatButton from 'material-ui/FlatButton';
 
-export default class App extends Component<any, any> {
+const fillStyle: CSSProperties = {
+  width: '100%',
+  height: '100%'
+};
+
+const welcomeScreenStyle: CSSProperties = {
+  display: 'flex',
+  flexFlow: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  ...fillStyle
+};
+
+const screens = [
+  <View key={0} style={welcomeScreenStyle}>1</View>,
+  <View key={1} style={welcomeScreenStyle}>2</View>,
+  <View key={2} style={welcomeScreenStyle}>3</View>,
+  <View key={3} style={welcomeScreenStyle}>4</View>
+];
+
+export default class App extends Component<any, {
+  index: number;
+}> {
+  state = {
+    index: 0
+  };
+
+  private changeIndex = (index: number) => this.setState({ index });
+
+  private prevScreen = () => this.state.index > 0 && this.changeIndex(this.state.index - 1);
+
+  private nextScreen = () => this.state.index < screens.length - 1 && this.changeIndex(this.state.index + 1);
+
   render() {
-    return <View fill flexFlow='column' style={{
+    const {index} = this.state;
+
+    return <View style={{
+      display: 'flex',
+      flexFlow: 'column',
       backgroundColor: theme.palette.primary1Color,
       alignItems: 'center',
-      justifyContent: 'center'
+      justifyContent: 'center',
+      ...fillStyle
     }}>
-      <View flex={1} style={{ width: '100%' }}>
-        <SwipeableViews style={{
-          width: '100%',
-          height: '100%'
-        }}>
-          <View fill>1</View>
-          <View fill>2</View>
-          <View fill>3</View>
+      <View style={{ flex: 1, width: '100%' }}>
+        <SwipeableViews index={index} onChangeIndex={this.changeIndex} style={fillStyle} containerStyle={fillStyle}>
+          {screens}
         </SwipeableViews>
       </View>
-      <View flexFlow='row' style={{
+      <View style={{
+        display: 'flex',
+        flexFlow: 'row',
         width: '100%',
         maxWidth: 480,
         alignItems: 'center',
         padding: 16
       }}>
-        <FlatButton label='Prev' />
-        <View flex={1} />
-        <FlatButton label='Prev' />
+        <FlatButton label='Prev' disabled={index === 0} onTouchTap={this.prevScreen} />
+        <View style={{ flex: 1 }} />
+        <FlatButton label='Next' disabled={index === screens.length - 1} onTouchTap={this.nextScreen} />
       </View>
     </View>;
   }
